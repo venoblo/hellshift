b9117bb27fa87d8bfd7f7a2b7e464b4d7625a058
MatheusChavesBecker <135650866+MatheusChavesBecker@users.noreply.github.com>
2025-11-05 21:15:40 -0300

    Algumas estruturas basicas

--- main.c
+++ main.c
@@ -1,23 +1,56 @@
 #include "raylib.h"
 
-typedef enum monstertype{
-     
+//ESTRUTURAS BASE E TIPOS **************************************************************
 
+typedef enum MonsterType{ // tipos de monstro, se for adicionar um novo monstro tem de por um desses aqui
+    MONSTER_SLIME,
+    MONSTER_SKELETON,
+    MONSTER_ZOMBIE
+}MonsterType
+
+
+typedef struct Player{ // jogador, aqui ficam suas caracteristicas
+    Vector2 position; // posição do jogador
+    float speed;      // velocidade de movimento
+    int life;         // vida
+    Color color;      // cor
+} Player;
+
+typedef struct Monster{ //qui são as caracteristicas base do tipo "Monstro", são o que vai ser editado par adiferenciar cada um, dps agnt muda isso para adicionar sprites e animações
+    Vector2 position;
+    MonsterType type;
+    int life;
+    Color color;
+}Monster;
+
+typedef struct Projectil{ //caracteristicas dos projeteis, msm historia
+    Vector2 position;
+    Vector2 direction;
+    float speed;
+    int active;
+}Projectil;
+
+// FUNÇÕES DO JOGADOR************************************************************
+
+void UpdatePlayer(Player *p) { //aqui são funções do jogador, por agora to pondo só movimento basico
+    if (IsKeyDown(KEY_W)) p->position.y -= p->speed;
+    if (IsKeyDown(KEY_S)) p->position.y += p->speed;
+    if (IsKeyDown(KEY_A)) p->position.x -= p->speed;
+    if (IsKeyDown(KEY_D)) p->position.x += p->speed;
 }
 
+void DrawPlayer(Player p) { // faz aparecer na tela, no momento ele vai ser essa bola msm
+    DrawCircleV(p.position, 12, p.color);
+}
 
-typedef struct player{
+// FUNÇÕES DOS MONSTROS *********************************************************
 
+void DrawMonster(Monster m) {
+    DrawCubeV(m.position, 15, m.color)
+}
 
-} player;
-
-typedef struct monster{
-
-}monster;
-
-typedef struct projectil{
-
-}projectil;
+// --------------------------------------------------------------------------------
+// PROGRAMA PRINCIPAL (MAIN) ******************************************************
 
 int main(void)
 {
@@ -26,23 +59,41 @@ int main(void)
     const int screenHeight = 450;
 
     // InitWindow cria a janela do jogo
-    InitWindow(screenWidth, screenHeight, "Meu Jogo Raylib [Mac/Linux]");
+    InitWindow(screenWidth, screenHeight, "Hellshift - Teste");
 
     SetTargetFPS(60); // Define o FPS (Frames Per Second)
+
+    // Criar jogador
+    Player player = {
+        .position = (Vector2){screenWidth/2, screenHeight/2},
+        .speed = 3.0f,
+        .life = 5,
+        .color = BLUE
+    };
+
+    //Criar monstro
+    Monster enemy = {
+        .position = (Vector2){200, 200},
+        .type = MONSTER_SLIME,
+        .life = 3,
+        .color = RED
+    };
 
     // 2. Loop Principal do Jogo
     while (!WindowShouldClose())    // Executa enquanto o usuário não fechar a janela
     {
-        // --- ATUALIZAÇÃO (Lógica do Jogo) ---
-        // (Seu código de lógica vai aqui)
+        // --- ATUALIZAÇÃO/UPDATE (Lógica do Jogo) ---
+        UpdatePlayer(&player);
 
-
-        // --- DESENHO (O que aparece na tela) ---
+        // --- DESENHO/DRAW (O que aparece na tela) ---
         BeginDrawing();
 
-            ClearBackground(RAYWHITE); // Limpa a tela com a cor branca
+            ClearBackground((WHITE){30, 30, 40, 255}); // Limpa a tela com a cor branca
 
-            DrawText("Parabéns! Sua primeira janela!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Hellshift - teste", 190, 200, 20, LIGHTGRAY);
+
+            DrawPlayer(player);
+            DrawMonster(enemy);
 
         EndDrawing();
     }
